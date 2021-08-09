639ade9929d363223721db428656a69624d25361
joharputraadek <joharputraadek@gmail.com>
2021-08-09 10:37:58 +0700

    test

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import MateriFlaxBox from './MateriFlaxBox';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containeeer}>
+      {/* <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Stext />
+      <BloxGreen /> */}
+      <ScrollView>
+        <MateriFlaxBox />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containeeer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const Stext = () => {
+  return <Text>Testijng</Text>;
+}
+
+class BloxGreen extends Component {
+  render() {
+    return <Text> blok green</Text>
+  }
+}
---
+++ MateriFlaxBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+
+class MateriFlaxBox extends Component {
+    render() {
+        return <View>
+            <Text>Materi MateriFlaxBox</Text>
+            <View style={{ backgroundColor: '#3cb371', flex: 2, height: 100, marginBottom: 0 }}></View>
+           
+        </View>
+    }
+}
+
+export default MateriFlaxBox;
