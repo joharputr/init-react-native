ef8c128feb18836dfaaccc8cc3182da6d2cfebbb
joharputraadek <joharputraadek@gmail.com>
2021-08-10 10:19:01 +0700

    add lifecycle

--- Position.js
+++ Position.js
@@ -1,24 +1,46 @@
-import { white } from "ansi-colors";
 import React, { Component } from "react";
 import { StyleSheet, View, Text, Image } from 'react-native';
 import cart from './cart.png';
 
 class Position extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buy: 80
+        }
+    }
+
     render() {
-        return <View style={styles.wrapper}>
+        return <View style={{ padding: 20, alignItems: 'center' }}>
             <Text>Position</Text>
             <View style={styles.cartWrapper}>
                 <Image source={cart} style={styles.iconCart}></Image>
-                <Text style={styles.notif}>10</Text>
+                <Text style={styles.notif}>{this.state.buy}</Text>
             </View>
         </View>
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                buy: 3000
+            });
+        }, 2000);
+    }
+
+    componentDidUpdate() {
+        console.log("update");
+    }
+
+    componentWillUnmount() {
+        console.log("unmount");
     }
 }
 
 export default Position;
 
+
 const styles = StyleSheet.create({
-    wrapper: { padding: 20, alignItems: 'center' },
     cartWrapper: {
         marginTop: 10,
         borderWidth: 2,
@@ -28,6 +50,7 @@ const styles = StyleSheet.create({
         borderRadius: 75 / 2,
         justifyContent: 'center',
         alignItems: 'center',
+        position: 'relative'
     },
     iconCart: {
         width: 50, height: 50
